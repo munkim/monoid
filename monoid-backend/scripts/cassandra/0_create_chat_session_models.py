a5118194e8bf92c635490dc686f3d11b3fba3612
Mun Kim <17050940+munkim@users.noreply.github.com>
2023-11-30 14:46:18 -0500

    Add a cassandra migration script

---
+++ monoid-backend/scripts/cassandra/0_create_chat_session_models.py
@@ -0,0 +1,65 @@
+"""
+Script to create tables on ScyllaDB (Cassandra)
+"""
+import os
+import boto3
+from pprint import pprint 
+from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from ssl import SSLContext, PROTOCOL_TLSv1_2 , CERT_REQUIRED
+from cassandra_sigv4.auth import SigV4AuthProvider
+from cassandra import ConsistencyLevel
+
+from monoid_backend.core.aws_keyspaces import get_cluster
+
+cluster = get_cluster()
+session = cluster.connect()
+
+
+print('Creating a keyspace...')
+session.execute(
+    "CREATE KEYSPACE IF NOT EXISTS monoid WITH REPLICATION = { \
+        'class' : 'SingleRegionStrategy' \
+    };"
+)
+print('Successfully created a keyspace!')
+
+
+print('\nCreating Tables')
+session.execute(
+    """
+    CREATE TABLE IF NOT EXISTS monoid.chat_session (
+        chat_session_uuid UUID,
+        account_uuid UUID,
+        agent_id INT,
+        title TEXT,
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP,
+        is_scheduled BOOLEAN,
+        PRIMARY KEY ((account_uuid, agent_id), chat_session_uuid)
+    ); 
+    """
+)
+print('Successfully created a table called chat_session')
+
+session.execute(
+    """
+    CREATE TABLE IF NOT EXISTS monoid.chat_message (
+        chat_session_uuid UUID,
+        message_uuid UUID,
+        account_uuid UUID,
+        agent_id INT,
+        role TEXT, 
+        content TEXT, 
+        message_type TEXT, 
+        action_name TEXT, 
+        content_length INT,
+        action_type TEXT,
+        message_author_name TEXT,
+        message_author_type TEXT,
+        nesting_level INT,
+        created_at TIMESTAMP,
+        PRIMARY KEY (chat_session_uuid, created_at)
+    ) WITH CLUSTERING ORDER BY (created_at ASC);
+    """
+)
+print('Successfully created a table called chat_message')
